6c4613fcbba2f0b1f7e8a7cf30f88f636cd93097
dwihandoko-studio <dwi.handoko0511@gmail.com>
2025-10-23 23:03:02 +0700

    fix home

--- Config/Routes.php
+++ Config/Routes.php
@@ -2,16 +2,16 @@
 
 use CodeIgniter\Router\RouteCollection;
 
-$routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
-$routes->setDefaultMethod('index');
-$routes->setTranslateURIDashes(false);
-$routes->set404Override();
-$routes->setAutoRoute(true);
+// $routes->setDefaultNamespace('App\Controllers');
+// $routes->setDefaultController('Home');
+// $routes->setDefaultMethod('index');
+// $routes->setTranslateURIDashes(false);
+// $routes->set404Override();
+// $routes->setAutoRoute(true);
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
 // $routes->get('/home', 'Home::index');
-$routes->get('/auth', 'Auth::index');
+// $routes->get('/auth', 'Auth::index');
 // $routes->get('/auth/login', 'Auth::login');
