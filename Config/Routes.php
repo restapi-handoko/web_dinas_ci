ba1a2134b1657ddc4a52c5df746e8a2d3f2a8d05
dwihandoko-studio <dwi.handoko0511@gmail.com>
2025-10-23 23:03:32 +0700

    fix home

--- Config/Routes.php
+++ Config/Routes.php
@@ -13,5 +13,5 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 // $routes->get('/home', 'Home::index');
-// $routes->get('/auth', 'Auth::index');
+$routes->get('/auth', 'Auth::index');
 // $routes->get('/auth/login', 'Auth::login');
