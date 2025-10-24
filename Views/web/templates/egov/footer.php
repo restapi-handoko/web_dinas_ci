76e9d9e783e0cb0112e82c4e4f892edd064974d3
dwihandoko-studio <dwi.handoko0511@gmail.com>
2025-10-24 20:55:43 +0700

    add informasi pengadaan

--- Views/web/templates/egov/footer.php
+++ Views/web/templates/egov/footer.php
@@ -7,26 +7,22 @@
                     <img class="" src="<?= base_url() ?>/assets/wp-content/images/logo-dark.png" alt="" width="300" />
 
                     <hr>
-                    <!-- <span class="text-light">
-                        Content Management System (CMS) DATAGOE dibuat khusus untuk situs pemerintahan, yayasan, sekolah, company profile dan lain-lain. CMS ini dibangun dengan Framework Codeigniter Versi 4.5.3 dan akan terus diupdate. </span> -->
                 </div>
                 <div class="col-md-6 pt-2">
                     <br>
                     <h4 class="text-white mt-2">Kontak Kami</h4>
-                    <span>Content Management System Datagoe | Jln. RS Bukit - Lembata Waikomo. Kabupaten Lembata, Provinsi: Nusa Tenggara Timur<br> No. Telp: +6281353967028 | Email : layanan@datagoe.com</span>
+                    <span><?= $footer->alamat ?><br> No. Telp: <?= $footer->telp ?> | Email : <?= $footer->email ?></span>
                     <div class="p-0 mt-2">
                         <div id="sprasa_about_us_widget-4" class="widget jellywp_about_us_widget">
                             <div class="widget_jl_wrapper about_widget_content">
                                 <div class="jellywp_about_us_widget_wrapper">
                                     <div class="social_icons_widget">
                                         <ul class="social-icons-list-widget icons_about_widget_display">
-                                            <li> <a href="https://www.facebook.com/datagoe" class="facebook" target="_blank"><i class="jli-facebook"></i></a>
+                                            <li> <a href="<?= $footer->facebook ?>" class="facebook" target="_blank"><i class="jli-facebook"></i></a>
                                             </li>
-                                            <li> <a href="https://www.youtube.com/c/datagoe" class="youtube" target="_blank"><i class="jli-youtube"></i></a>
+                                            <li> <a href="<?= $footer->instagram ?>" class="instagram" target="_blank"><i class="jli-instagram"></i></a>
                                             </li>
-                                            <li> <a href="https://instagram.com/datagoe" class="instagram" target="_blank"><i class="jli-instagram"></i></a>
-                                            </li>
-                                            <li> <a href="http://twitter.com/datagoe" class="twitter" target="_blank"><i class="jli-twitter"></i></a>
+                                            <li> <a href="<?= $footer->twitter ?>" class="twitter" target="_blank"><i class="jli-twitter"></i></a>
                                             </li>
                                             </a>
                                             </li>
@@ -53,14 +49,7 @@
                                 <a class="text-light mb-1" target="_parent" href="page/redaksi.html" style="font-size: 14px;"> <i class=""></i> Redaksi | </a>
                                 <a class="text-light mb-1" target="_parent" href="page/syarat-dan-kondisi.html" style="font-size: 14px;"> <i class=""></i> Syarat & Kondisi | </a>
                                 <a class="text-light mb-1" target="_parent" href="petasitus.html" style="font-size: 14px;"> <i class=""></i> Peta Situs | </a>
-                                <a style="font-size: 14px;color:#fff;"> &copy; 2025 - Content Management System Datagoe </a>
-                                <br>
-
-                                <i> <a style="font-size: 14px;color:#fff;"> Dibuat dengan <i class="mdi mdi-heart text-danger"></i> Oleh <a href="https://datagoe.com" target="_blank"> Datagoe Software </a> </a>
-                                </i>
-                                &nbsp;| <a class="pb-0" style="font-size: 14px;color:#fff; padding-bottom: 0pt;">Online 1 Orang</a>
-
-
+                                <a style="font-size: 14px;color:#fff;" target="_blank" href="http://diskominfo.lampungtengahkab.go.id"> &copy; <?= date('Y'); ?> - iskominfo Kab. Lampung Tengah </a>
                         </center>
                     </div>
                 </div>
