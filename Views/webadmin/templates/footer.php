d68dfd939eba60030e6f35657fcc71b398f92f4d
dwihandoko-studio <dwi.handoko0511@gmail.com>
2025-10-25 00:27:21 +0700

    add informasi pengadaan

--- Views/webadmin/templates/footer.php
+++ Views/webadmin/templates/footer.php
@@ -4,7 +4,7 @@
             <div class="col-sm-6">
                 <script>
                     document.write(new Date().getFullYear())
-                </script> © Disdikbud Kab. Lampung Tengah.
+                </script> © Bagian Pengadaan Barang Dan Jasa Kab. Lampung Tengah.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
