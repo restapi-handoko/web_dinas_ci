d470b2eb5235b6d0ff3348cd810754d57a132a06
dwihandoko-studio <dwi.handoko0511@gmail.com>
2025-10-24 15:46:28 +0700

    add informasi pengadaan

--- Filters/Filterauth.php
+++ Filters/Filterauth.php
@@ -32,7 +32,7 @@ class Filterauth implements FilterInterface
                     $uri = current_url(true);
                     $totalSegment = $uri->getTotalSegments();
                     if ($totalSegment > 0) {
-                        var_dump($uri->getSegment(0));
+                        var_dump($uri->getSegment(1));
                         die;
                         $uriMain = $uri->getSegment(1);
 
