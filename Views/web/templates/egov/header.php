4e77a96f9740c91f4017b2b23a7f26cd0db53c33
dwihandoko-studio <dwi.handoko0511@gmail.com>
2025-10-26 15:24:19 +0700

    pembenahan home

--- Views/web/templates/egov/header.php
+++ Views/web/templates/egov/header.php
@@ -113,8 +113,7 @@
                                 $menuPublik = getMenuPublik(); ?>
                                 <?php if (count($menuPublik) > 0) { ?>
                                     <?php foreach ($menuPublik as $key => $value) { ?>
-                                        <?php if ((int)$value->has_sub == 1) {
-                                        ?>
+                                        <?php if ((int)$value->has_sub == 1) { ?>
                                             <li class="menu-item menu-item-has-children">
                                                 <a href="#"><?= $value->judul ?><span class="border-menu"></span></a>
                                                 <ul class="sub-menu">
@@ -122,16 +121,22 @@
                                                     $menuSubPublik = getSubMenuPublik($value->id); ?>
                                                     <?php if (count($menuSubPublik) > 0) { ?>
                                                         <?php foreach ($menuSubPublik as $keyV => $v) { ?>
-                                                            <li class="menu-item current-menu-item current_page_item"><a target="_parent" href="<?= $v->url ?>"><?= $v->icon ?? '' ?><?= $v->judul ?></a></li>
+                                                            <?php if ((int)$value->external_link == 1) { ?>
+                                                                <li class="menu-item current-menu-item current_page_item"><a target="_blank" href="<?= $v->url ?>"><?= $v->icon ?? '' ?><?= $v->judul ?></a></li>
+                                                            <?php } else { ?>
+                                                                <li class="menu-item current-menu-item current_page_item"><a target="_parent" href="<?= $v->url ?>"><?= $v->icon ?? '' ?><?= $v->judul ?></a></li>
+                                                            <?php } ?>
                                                         <?php } ?>
                                                     <?php } ?>
                                                 </ul>
                                             </li>
-                                        <?php } else {
-                                        ?>
-                                            <li class="menu-item current-menu-item current_page_item"><a target="_parent" href="<?= $value->url ?>"> <?= $value->icon ?? '' ?><?= $value->judul ?><span class="border-menu"></span> </a></li>
-                                        <?php }
-                                        ?>
+                                        <?php } else { ?>
+                                            <?php if ((int)$value->external_link == 1) { ?>
+                                                <li class="menu-item current-menu-item current_page_item"><a target="_blank" href="<?= $value->url ?>"> <?= $value->icon ?? '' ?><?= $value->judul ?><span class="border-menu"></span> </a></li>
+                                            <?php } else { ?>
+                                                <li class="menu-item current-menu-item current_page_item"><a target="_parent" href="<?= $value->url ?>"> <?= $value->icon ?? '' ?><?= $value->judul ?><span class="border-menu"></span> </a></li>
+                                            <?php } ?>
+                                        <?php } ?>
                                     <?php } ?>
                                 <?php } ?>
                             </ul>
